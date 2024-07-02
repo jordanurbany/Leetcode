681147bfad0d002bf0ee329f3d4372f2569789c9
Jordan Urbany <77860041+jordanurbany@users.noreply.github.com>
2024-07-01 22:47:58 -0700

    added to ReactButtons

--- ReactButtons.jsx
+++ ReactButtons.jsx
@@ -1,25 +1,60 @@
 import React, { children, useState } from "react";  
 
-
-const Button = ({onClick, children}) {
-    return ( 
+//simple button
+const Button = ({children, onClick}) => {
+    return (
         <button onClick={onClick}>
-        {children}
+            {children}
         </button>
-
     )
-};
+}
+
+
 //how to build a simple counter
-const Counter = () {
-    const [count, setCount] = useState(0);
-    return(
-        <div>
-        <h1>you clicked {count} times</h1>
-        <button onClick={()=> setCount(count + 1)}>click me</button>
-        </div>
-    );
-};
+const Counter = () => {
+        const [count, setCount] = useState(0);
+        return (
+            <div className="tailwind">
+              <button onclick={()=> setCount(count + 1)}/>Click me</button>  
+            </div>
+        );
+    };
 //form handling
+const Form = () => {
+    const [name, setName] = useState('');
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        alert('form submitted with a name: ${name}')
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                name:
+                <input type="text" value={name} onchange={handleChange} />
+                <button type="submit">submit</button>
+            </label>
+        </form>
+    )
+}
+
+
+//how to make a simple toggel switch
+const Toggle = () => (
+    const [isOn, setIsOn] = useState(false);
+    return (
+        <div className="toggle">
+            <button onClick={() => setIsOn(!isOn)}>
+            {isOn ? 'Turn off' : "turn on"}
+            </button>
+            {isOn && <p>the swith is on</p>}
+        </div>
+    )
+
+);
+
 
 
 
@@ -32,4 +67,4 @@ const App = () => {
 
 
 };
-export default App
+export default App;
